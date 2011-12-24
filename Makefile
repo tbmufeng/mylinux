c2b66d70862db818c43fd9aac80296a1188c48e3
tbmufeng <qcgang1987@163.com>
2011-12-24 22:45:50 +0800

    helloworld

--- Makefile
+++ Makefile
@@ -1,3 +1,11 @@
-# 光盘启动的 应用
-test.iso : test
-mkisofs -o mytest.iso boot/
+#光盘启动应用
+
+Image : boot.o
+	cd boot; cat boot.bin > ../mkoutput/boot.bin
+	cdimage -bmkoutput/boot.bin  mkoutput/ Image2.iso
+boot.o	:	boot/boot.s
+	nasm boot/boot.s -f bin -o boot/boot.bin
+clean	:
+	rm boot/boot.o 
+	rm mkoutput/boot.bin	
+	
--- boot/boot.s
+++ boot/boot.s
@@ -1,10 +1,56 @@
 ;hang.asm
+
+mov ax,07c0h;设置堆栈
+add ax,288
+mov ss,ax
+mov sp,4096
+mov ax,07c0h
+mov ds,ax
 hang:
-mov ah,0x0e
-mov al,01
-mov bl,07
-int 10
-end:
+mov si,welcome
+call println
+call delay
 jmp hang
-times 510-($hang-$end)  db 0;
-dw 0AA55h
+
+println: ;字符串地址存放到bx
+	push ax
+	push bx
+	mov ah,0eh
+repeat
+	lodsb ;获取字符
+	cmp al,0
+	je	printend
+	int 10h
+	jmp repeat
+
+printend:
+	mov al,0dh ;回车
+	int 10h
+	mov al,0ah ;换行
+	int 10h
+	pop bx
+	pop ax
+	ret
+
+delay:
+	push ax
+	push bx
+	mov ax, 0ffffh
+delay_repeat_1:
+	dec ax
+	cmp ax,0
+	je delay_end
+	mov bx,01ffh
+delay_repeat_2:
+	dec bx
+	cmp bx,0
+	je delay_repeat_1
+	jmp delay_repeat_2
+delay_end:
+	pop bx
+	pop ax
+	ret
+
+welcome: db 'hello world ',0
+times 510-($-$$)  db 0;
+dw 0xAA55
